e08ea714c7a66dd3a7de90da0080ebdecb5a5898
JoseGm56 <josegonzamac4@gmail.com>
2024-01-07 19:24:44 -0600

    Finalized Heat Map

--- Heat_Map/HeatMap.js
+++ Heat_Map/HeatMap.js
@@ -27,7 +27,30 @@ d3.json(url).then(function(rawData){
                 longitude = Number(key.split(",")[1])
                 data.push([latitude, longitude, intensity])
             };
-            console.log(data)
+            //console.log(data)
+            return(data)      
+        };
+        if (demographic === "Year Missing"){
+            for (let i = 0; i < dataset.length; i++){
+                city_coor = [dataset[i].Latitude, dataset[i].Longitude]
+                if (dataset[i][demographic] >= specific && dataset[i][demographic] < (specific + 10)){
+                demo_list.push(city_coor)
+            };
+        };
+        demo_list.sort();
+        demo_list.forEach(function(i) { intensity_dic[i] = (intensity_dic[i]||0) + 1;});
+        keys = Object.keys(intensity_dic)
+        //console.log(demo_list, specific)
+
+            data = []
+            for(let i= 0; i<keys.length; i++){
+                key = keys[i]
+                intensity = intensity_dic[key]
+                latitude = Number(key.split(",")[0])
+                longitude = Number(key.split(",")[1])
+                data.push([latitude, longitude, intensity])
+            };
+            //console.log(data)
             return(data)      
         };
         for (let i = 0; i < dataset.length; i++){
@@ -139,6 +162,60 @@ d3.json(url).then(function(rawData){
         minOpacity: .5
     });
 
+    let the2020s = L.heatLayer(
+        perCity("Year Missing", 2020),
+        {radius: 15,
+        minOpacity: .5
+    });
+    let the2010s = L.heatLayer(
+        perCity("Year Missing", 2010),
+        {radius: 15,
+        minOpacity: .5
+    });
+    
+    let the2000s = L.heatLayer(
+        perCity("Year Missing", 2000),
+        {radius: 15,
+        minOpacity: .5
+    });
+    
+    let the1990s = L.heatLayer(
+        perCity("Year Missing", 1990),
+        {radius: 15,
+        minOpacity: .5
+    });
+    
+    let the1980s = L.heatLayer(
+        perCity("Year Missing", 1980),
+        {radius: 15,
+        minOpacity: .5
+    });
+
+        
+    let the1970s = L.heatLayer(
+        perCity("Year Missing", 1970),
+        {radius: 15,
+        minOpacity: .5
+    });
+        
+    let the1960s = L.heatLayer(
+        perCity("Year Missing", 1960),
+        {radius: 15,
+        minOpacity: .5
+    });
+
+    let the1950s = L.heatLayer(
+        perCity("Year Missing", 1950),
+        {radius: 15,
+        minOpacity: .5
+    });
+
+    let the1940s = L.heatLayer(
+        perCity("Year Missing", 1940),
+        {radius: 15,
+        minOpacity: .5
+    });
+
     let map = L.map('heatmap', {
         center: [30.16, -97.45],
         zoom: 6.2,
@@ -163,7 +240,16 @@ d3.json(url).then(function(rawData){
         "Black": raceB,
         "White": raceW,
         "Asian": raceA,
-        "Hispanic": raceH
+        "Hispanic": raceH,
+        "40's - 1 Cases": the1940s,
+        "50's - 2 Cases": the1950s,
+        "60's - 2 Cases": the1960s,
+        "70's - 35 Cases": the1970s,
+        "80's - 144 Cases": the1980s,
+        "90's - 176 Cases": the1990s,
+        "00's - 242 Cases": the2000s,
+        "10's - 197 Cases": the2010s,
+        "20's - 42 Cases": the2020s,
     };
 
     let layerControl = L.control.layers(overlayMaps).addTo(map);
